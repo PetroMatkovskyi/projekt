3e3b49ec038c6be88332a41b39f626699a563e3d
PetroMatkovskyi <petromatkovskij3021@gmail.com>
2020-12-22 22:57:52 +0000

    Add carousel on insta and section awards

--- js/index.js
+++ js/index.js
@@ -2,10 +2,6 @@ const mainMenu = document.querySelector("#main");
 const vegetarianMenu = document.querySelector("#vegetarian");
 const drinksMenu = document.querySelector("#drinks");
 
-console.log(mainMenu);
-console.log(vegetarianMenu);
-console.log(drinksMenu);
-
 function showMainMenu() {
   mainMenu.classList.remove("displayOff");
   vegetarianMenu.classList.add("displayOff");
@@ -28,10 +24,53 @@ const mainButton = document.querySelector(".main-button");
 const vegetarianButton = document.querySelector(".vegetarian-button");
 const drinksButton = document.querySelector(".drinks-button");
 
-console.log(mainButton);
-console.log(vegetarianButton);
-console.log(drinksButton);
-
 mainButton.addEventListener("click", showMainMenu);
 vegetarianButton.addEventListener("click", showVegetarianMenu);
 drinksButton.addEventListener("click", showDrinksMenu);
+
+//Carousel
+
+{
+  const images = [
+    "img/on_insta/fire-burger.jpg",
+    "img/on_insta/delicious-burger.jpg",
+    "img/on_insta/beautiful_view.jpg",
+    "img/on_insta/big-burgers.png",
+    "img/on_insta/black_burger.jpg",
+    "img/on_insta/on_the_beach.jpg",
+    "img/on_insta/grilled_burgers.jpg",
+    "img/on_insta/friendly_meeting.jpg",
+  ];
+
+  let currentIdx = 0;
+  function showCurrent() {
+    const imgElement1 = document.querySelector(".all_insta_photo .img1");
+    const imgElement2 = document.querySelector(".all_insta_photo .img2");
+    const imgElement3 = document.querySelector(".all_insta_photo .img3");
+    const imgElement4 = document.querySelector(".all_insta_photo .img4");
+    const idx2 = currentIdx + 1 >= images.length ? 0 : currentIdx + 1;
+    const idx3 = idx2 + 1 >= images.length ? 0 : idx2 + 1;
+    const idx4 = idx3 + 1 >= images.length ? 0 : idx3 + 1;
+    imgElement1.src = images[currentIdx];
+    imgElement2.src = images[idx2];
+    imgElement3.src = images[idx3];
+    imgElement4.src = images[idx4];
+  }
+
+  function showNext() {
+    currentIdx++;
+    if (currentIdx >= images.length) currentIdx = 0;
+    showCurrent();
+  }
+
+  function showPrev() {
+    currentIdx--;
+    if (currentIdx < 0) currentIdx = images.length - 1;
+    showCurrent();
+  }
+
+  setInterval(showNext, 3000);
+  document.querySelector(".next").addEventListener("click", showNext);
+  document.querySelector(".prev").addEventListener("click", showPrev);
+  showCurrent();
+}
